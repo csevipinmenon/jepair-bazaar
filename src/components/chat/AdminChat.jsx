e92f772e31fdcbe0c99ae7a68d5dd59e318b9695
Vipin kumar <166358641+csevipinmenon@users.noreply.github.com>
2025-07-22 22:09:13 +0530

    Update AdminChat.jsx

--- src/components/chat/AdminChat.jsx
+++ src/components/chat/AdminChat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://jepairbackend.vercel.app/");
+const socket = io("https://jepairbackend.onrender.com/");
 
 function AdminChat() {
   const [selectedUser, setSelectedUser] = useState("");
