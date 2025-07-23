e98255fd11381c0a6f4a3f21d2df0b2b94ec733f
Vipin kumar <166358641+csevipinmenon@users.noreply.github.com>
2025-07-23 15:51:17 +0530

    Update UserChat.jsx

--- src/components/chat/UserChat.jsx
+++ src/components/chat/UserChat.jsx
@@ -48,17 +48,17 @@ function UserChat() {
 
   return (
     <>
-      <div className="w-full flex justify-center items-center shadow-lg">
+      <div className="w-full flex justify-center items-center shadow-lg ">
         <h1 className="absolute text-3xl text-[#3a76cb]   font-extrabold ">
           Welcome To Chat!
         </h1>
         <img src="jepairBanner.jpg" className="h-[200px] w-full" />
       </div>
-      <div className="p-4 ">
+      <div className="p-4  dark:bg-[#343a46]">
         <h2 className="text-3xl shadow-lg border-b-2 border-dashed py-4    text-blue-500 border-orange-500 font-extrabold mb-2 text-center">
           <span className="  rounded-md">
             {" "}
-            Jepair <span className="text-orange-500  bg-white">Bazaar </span>
+            Jepair <span className="text-orange-500  bg-white dark:bg-[#343a46]">Bazaar </span>
           </span>
           <span class="relative flex h-5 w-5  left-1/2 bottom-14">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
@@ -68,7 +68,7 @@ function UserChat() {
 
         <div
           ref={chatRef}
-          className="h-80 shadow-lg overflow-y-auto border mt-8 rounded-md bg-[#f2f2f2] p-2 mb-3"
+          className="h-80 shadow-lg overflow-y-auto border mt-8 rounded-md dark:bg-gray-800 dark:border-blue-500 bg-[#f2f2f2] p-2 mb-3"
         >
           {chatLog.map((msg, idx) => (
             <div
@@ -98,7 +98,7 @@ function UserChat() {
         <div className="flex gap-2 mb-36 mt-12">
           <input
             type="text"
-            className="border p-2 w-full focus:outline-none  border-blue-500  rounded-lg focus:border-orange-500"
+            className="border p-2 w-full focus:outline-none  border-blue-500 dark:bg-gray-800   rounded-lg focus:border-orange-500"
             placeholder="Type your message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
